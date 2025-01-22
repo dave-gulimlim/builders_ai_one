655b75873dacf82bb0426adc67657e2e9613ce98
Dae Gulimlim <davegulimlim@gmail.com>
2025-01-22 20:18:19 +0000

    Created a button in the middle

---
+++ config.js
@@ -0,0 +1,6 @@
+const config {
+    parse_app_id : "4qJDQvbuvEjy0e7bBrCVB48hp75wyTXylaO1IXwX"
+    parse_js_id : "TSrYYHfGJQQG6peGo4Ks7I5xCD1AUYkY7m8WQDWg"
+    parse_resume_class : "resume";
+    parse_resume_column_name : "pdf";
+}
